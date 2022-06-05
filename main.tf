92b9073df2c935b8e5d0a2d20f2e6f76ec8c6787
tp199314 <tobias.prein@googlemail.com>
2022-06-05 17:07:47 +0200

    var fix #2

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ resource "random_pet" "rg-name" {
 
 resource "azurerm_resource_group" "rg" {
   name     = random_pet.rg-name.id
-  location = var.resource_group_location
+  location = var.location
 }
 
 # Create virtual network
